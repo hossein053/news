088f418d466e9df214cb65d34f4ac04c79a2ab42
Hossein <hosseinbohlol8520@gmail.com>
2025-01-03 19:54:04 +0330

    domains

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,15 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      // {
-      //   protocol: 'http',
-      //   hostname: 'localhost',
-      //   port: '3000',
-      //   pathname: '/uploads/**'
-      // },
-      {
-        protocol: 'https',
-        hostname: 'news-indol-omega.vercel.app',
-        port: '3000',
-        pathname: '/uploads/**'
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'news-indol-omega.vercel.app',
+    //     port: '3000',
+    //     pathname: '/uploads/**'
+    //   },
+    // ],
+    domains: ['news-indol-omega.vercel.app'],
     loader: 'default',
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384]
