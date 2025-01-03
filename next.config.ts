6303a6c50df9e4a6270d4164b1bb983c88989bb6
Hossein <hosseinbohlol8520@gmail.com>
2025-01-03 19:51:18 +0330

    protocol

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,18 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // {
+      //   protocol: 'http',
+      //   hostname: 'localhost',
+      //   port: '3000',
+      //   pathname: '/uploads/**'
+      // },
       {
-        protocol: 'http',
-        hostname: 'localhost',
+        protocol: 'https',
+        hostname: 'news-indol-omega.vercel.app',
         port: '3000',
         pathname: '/uploads/**'
-      }
+      },
     ],
     loader: 'default',
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
